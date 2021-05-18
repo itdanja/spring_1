0fc4e3f6b312521ec7c3979609cbe310a729c490
Sera Lee <issell@naver.com>
2021-05-18 18:55:19 +0900

    20210518

--- src/main/java/spring/web/IndexController.java
+++ src/main/java/spring/web/IndexController.java
@@ -1,17 +1,27 @@
 package spring.web;
 
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import spring.service.MemberService;
+import spring.web.dto.MemberDto;
 
-@Controller
+import java.util.List;
+
+
+@Controller //  해당 클래스 컨트롤러 주입
+@RequiredArgsConstructor
 public class IndexController {
-
-    // Html 클라이언트에게 주기
-    @GetMapping("/")  // 요청 rul 만들기  //   / : ip주소:port명
-    public String index(){
-        return "index"; // 문자열 index  ===> html 파일명
-            // 타임리프 설치시 : templates 폴더에서 해당 html 파일명을 찾아서 돌려줌
-
+    private final MemberService memberService ;
+    @GetMapping("/") // 요청 URL 만들기  [ ip주소:port번호 ]
+    public String index(Model model){ // 메소드
+        // 회원목록 서비스
+        List<MemberDto> memberDtoList = memberService.memberlist();
+        // html에 데이터 전송 => model.addAttribute( "모델명" , 데이터 ) ;
+        model.addAttribute( "memberDtoList" , memberDtoList);
+        return "index"; //  문자열 반환
+            // 타임리프가 html 를 찾는위치 : resources -> templates
     }
-
 }
