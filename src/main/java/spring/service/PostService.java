c227ec4df27b1ba778fecbc54b2bff482431f3d8
Sera Lee <issell@naver.com>
2021-05-25 18:59:17 +0900

    2021-05-25

--- src/main/java/spring/service/PostService.java
+++ src/main/java/spring/service/PostService.java
@@ -2,6 +2,9 @@ package spring.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import spring.domain.post.PostEntity;
 import spring.domain.post.PostRepository;
@@ -24,25 +27,34 @@ public class PostService {
         postRepository.save( postDto.toEntity() );
     }
     // 2. 게시물 모든 출력
-    public List<PostDto> postlist(){
-        // 모든 entity 반환
-        List<PostEntity> postEntities =  postRepository.findAll();
-        // 모든 entity -> 모든 dto
-        List<PostDto> postDtos = new ArrayList<>();
+    public Page<PostEntity> postlist(Pageable pageable){
 
-        for( PostEntity temp : postEntities ){
-
-            PostDto postDto = PostDto.builder()
-                    .id(temp.getId())
-                    .title(temp.getTitle())
-                    .contents(temp.getContents())
-                    .name(temp.getName())
-                    .count(temp.getCount())
-                    .createDate( temp.getCreateDate())
-                    .build();
-            postDtos.add( postDto);
-        }
-        return postDtos;
+        // 현재 페이지
+        int page = (pageable.getPageNumber() == 0 ) ? 0 : (pageable.getPageNumber()-1);
+                        // 논리 ? 참[T] : 거짓[F]
+        // 현재 페이지 설정
+        pageable = PageRequest.of( page , 5 , new Sort( Sort.Direction.DESC , "id"));
+                    // PageRequest.of( 현재페이지 , 페이지당 게시글수 , sort )
+        // 현재 페이지의 게시물 찾기
+        return postRepository.findAll( pageable );
+                //        // 모든 entity 반환
+                //        List<PostEntity> postEntities =  postRepository.findAll();
+                //        // 모든 entity -> 모든 dto
+                //        List<PostDto> postDtos = new ArrayList<>();
+                //
+                //        for( PostEntity temp : postEntities ){
+                //
+                //            PostDto postDto = PostDto.builder()
+                //                    .id(temp.getId())
+                //                    .title(temp.getTitle())
+                //                    .contents(temp.getContents())
+                //                    .name(temp.getName())
+                //                    .count(temp.getCount())
+                //                    .createDate( temp.getCreateDate())
+                //                    .build();
+                //            postDtos.add( postDto);
+                //        }
+                //        return postDtos;
     }
 
     // 3. 게시물 개별 출력
@@ -76,8 +88,25 @@ public class PostService {
     }
 
     // 4. 게시물 수정
+    @Transactional
+    public void postupdate( PostDto updateDto ){
 
+        // 1. 해당 엔티티 찾기
+        Optional<PostEntity> optionalPostEntity = postRepository.findById( updateDto.getId() );
+        // 2. 엔티티 가져오기
+        PostEntity postEntity = optionalPostEntity.get();
+        // 3. 엔티티 수정처리
+        postEntity.update( updateDto );
+
+    }
     // 5. 게시물 삭제
-
+    public void postdelete( Long id ){
+        // 1. 엔티티 찾기
+        Optional<PostEntity> optionalPostEntity = postRepository.findById(id);
+        // 2. 엔티티 가져오기
+        PostEntity postEntity = optionalPostEntity.get();
+        // 3. 삭제 처리
+        postRepository.delete( postEntity );
+    }
 
 }
--- src/main/java/spring/web/PostController.java
+++ src/main/java/spring/web/PostController.java
@@ -1,11 +1,15 @@
 package spring.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import spring.domain.post.PostEntity;
 import spring.service.PostService;
 import spring.web.dto.PostDto;
 
@@ -19,10 +23,19 @@ public class PostController {
 
     // 1. 게시판 페이지 요청
     @GetMapping("/postlist")
-    public String postlist(Model model){  // 모델 : 프론트에게 데이터 전달
-        List<PostDto> postDtos = postService.postlist();
-        model.addAttribute( "postDtos" , postDtos); // 모델 이름 , 모델에 넣을 변수/객체
+    public String postlist(Model model , @PageableDefault Pageable pageable){  // 모델 : 프론트에게 데이터 전달
+
+        Page<PostEntity> postEntities = postService.postlist( pageable );
+
+        model.addAttribute( "postDtos" , postEntities );
+
         return "postlist";
+
+//        List<PostDto> postDtos = postService.postlist();
+//        model.addAttribute( "postDtos" , postDtos); // 모델 이름 , 모델에 넣을 변수/객체
+//        return "postlist";
+
+
     }
     // 2. 게시물 등록 페이지 요청
     @GetMapping("/postwrite")
@@ -41,19 +54,36 @@ public class PostController {
     @GetMapping("/postview/{id}")
     public String postview(@PathVariable Long id  , Model model ) {
                            // 경로(url) 상에 변수 가져오기
-        PostDto postDto = postService.postget( id );
-        model.addAttribute( "postDto" , postDto);
         // 조회수 처리
         postService.countup(id);
+
+        PostDto postDto = postService.postget( id );
+        model.addAttribute( "postDto" , postDto);
 
         return "postview";
 
     }
     // 4. 게시물 수정 페이지 요청
-
+    @GetMapping("/postupdate/{id}")
+    public String postupdate(@PathVariable Long id , Model model ){
+        PostDto postDto =  postService.postget(id);
+        model.addAttribute("postDto" , postDto);
+        return "postupdate";
+    }
     // 5. 게시물 수정 처리
+    @PostMapping("/postupdate")
+    public String postupdate_c( PostDto updatedto ){
 
+        postService.postupdate(updatedto);
+        return "redirect:/postlist"; // URL 요청
+
+    }
     // 6. 게시물 삭제 처리
+    @GetMapping("/postdelete/{id}")
+    public String postdelete( @PathVariable Long id ){
+        postService.postdelete(id);
+        return "redirect:/postlist"; // URL 요청
+    }
 
 
 
